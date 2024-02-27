de57979ae9cb629fbda8a4956e133e43a64586d6
kiswaafaq <kiswaafaque14@gmail.com>
2024-02-27 15:06:36 -0800

    First commit

---
+++ 14. Guest List.ts
@@ -0,0 +1,7 @@
+let guest_list: string[] = ['Kiswa','Uzma','Ajia'];
+
+for(let i=0; i<guest_list.length; i++){
+
+    console.log('Dear Ms.' + guest_list[i] + ',\n\n\n It is our pleasure to invite you to our party. \n\n Thankyou!');
+
+}
---
+++ guest_list.js
@@ -0,0 +1,5 @@
+"use strict";
+let guest_list = ['Kiswa', 'Uzma', 'Ajia'];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Dear Ms.' + guest_list[i] + ',\n\n\n It is our pleasure to invite you to our party. \nThankyou!\n\n');
+}
